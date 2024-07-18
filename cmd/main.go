40d726c6cd935a5da7f6d94685975afffa6a2906
Vlad <zhevnyakvlig.1.23@gmail.com>
2024-07-18 18:31:18 +0300

    Create some handlers

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,13 @@ import (
 	"log"
 
 	todo "github.com/VladislavZhevnyak/ToDo-app"
+	"github.com/VladislavZhevnyak/ToDo-app/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(todo.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutees()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 }
