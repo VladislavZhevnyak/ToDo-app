84f0bc78a34755124b2d589f13bac79529bfe302
Vlad <zhevnyakvlig.1.23@gmail.com>
2024-07-23 22:04:12 +0300

    Add repo and services

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,15 @@ import (
 
 	todo "github.com/VladislavZhevnyak/ToDo-app"
 	"github.com/VladislavZhevnyak/ToDo-app/pkg/handler"
+	"github.com/VladislavZhevnyak/ToDo-app/pkg/repository"
+	"github.com/VladislavZhevnyak/ToDo-app/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutees()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
